6655a649849b83a3fe93a45f32f34d1dceef895a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-19 12:15:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 function produceDrivingRange(blocks) {
   return function(start, end) {
-    return parseint(end.slice(0,-1)) - parseint(start.slice(0,-1));
+    let result = (parseint(end.slice(0,-2)) - parseint(start.slice(0,-2)))-blocks;
+    result < 0 ? return '${result} blocks out of range' : return 'within range by ${result}';
   }
 }
 
@@ -12,3 +13,8 @@ function produceTipCalculator() {
 function createDriver() {
 
 }
+
+let eightBlockRange = produceDrivingRange(8)
+console.log(eightBlockRange('10th', '20th'));
+('2 blocks out of range')
+expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
