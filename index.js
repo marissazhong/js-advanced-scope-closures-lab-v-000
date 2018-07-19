835f94ac61a0e6092c730e32d25875a74418bb83
Marissa Zhong <mwz2101@columbia.edu>
2018-07-19 12:24:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function produceDrivingRange(blocks) {
+  return function(start, end) {
+    let result = (parseInt(end.slice(0,-2)) - parseInt(start.slice(0,-2)))-blocks;
+    return result < 0 ? `within range by ${Math.abs(result)}` : `${result} blocks out of range`;
+  }
+}
+
+function produceTipCalculator(percent) {
+  return function(bill) {
+    return bill * percent;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
+
+// let eightBlockRange = produceDrivingRange(8)
+// console.log(eightBlockRange('10th', '20th'));
+// console.log(eightBlockRange('10th', '14th'));
